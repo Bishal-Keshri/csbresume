306242c8a7b76924911ea1cca4a58c740299ec09
Bibsal <bishalprasadkeshri@gmail.com>
2025-11-07 04:33:40 +0530

    updating personal details

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Smooth scroll
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
